39e42ae93c650b3ca67bcdbaf94ac2266d55883d
britzinger <46625337+britzinger@users.noreply.github.com>
2022-05-04 07:58:16 -0400

    Initial

---
+++ fconfig.py
@@ -0,0 +1,21 @@
+# fconfig.py
+CHORD_WIDTH = 1
+
+Ginput_file = 'Partitions_new.xlsx'
+
+color_names = {
+1:('#00FFFF','aqua'),
+2:('#FF6103','cadmiumorange'),
+3:('#68228B','darkorchid4'),
+4:('#FF69B4','hotpink'),
+5:('#FF3030','firebrick1'),
+6:('#808080','gray'),
+7:('#1B45F1','blue'),
+8:('#FFFF00','yellow1'),
+9:('#000000','black'),
+10:('#8B2323','brown4'),
+11:('#006400','darkgreen'),
+12:('#00C957','emeraldgreen'),
+13:('#87CEFA','lightskyblue'),
+14:('#BCA8E2','lightpurple'),
+}
