ca6923051282069e14f5ee8accd78e8e001653bc
britzinger <ritz.b.2020@gmail.com>
2022-05-05 08:19:30 -0400

    color bar changes and debug code

--- fconfig.py
+++ fconfig.py
@@ -1,5 +1,6 @@
 # fconfig.py
 CHORD_WIDTH = 1
+INTERSPACE = 5 # spaces between partitions on plot
 
 Ginput_file = 'Partitions_new.xlsx'
 
@@ -19,3 +20,22 @@ color_names = {
 13:('#87CEFA','lightskyblue'),
 14:('#BCA8E2','lightpurple'),
 }
+
+
+# https://matplotlib.org/3.5.0/gallery/color/named_colors.html
+unh_colors1 = {
+    1: 'deepskyblue',
+    2: 'lightsalmon',
+    3: 'purple',
+    4: 'hotpink',
+    5: 'red',
+    6: 'gray',
+    7: 'blue',
+    8: 'yellow',
+    9: 'black',
+    10: 'saddlebrown',
+    11: 'darkgreen',
+    12: 'limegreen',
+    13: 'lightskyblue',
+    14: 'thistle'
+ }
